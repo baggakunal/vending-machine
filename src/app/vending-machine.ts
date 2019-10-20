b778bcdc3266da4fc81feca6f6dacb61daba9d99
baggakunal <baggakunal93@gmail.com>
2019-10-21 02:53:55 +0530

    connecting to UI

--- src/app/vending-machine.ts
+++ src/app/vending-machine.ts
@@ -1,3 +1,14 @@
-class VendingMachine {
+/// <reference path="./coin.ts" />
 
+
+class VendingMachine {
+    private paid: number = 0;
+
+    acceptCoin = (coin: Quarter): void => {
+        this.paid += coin.value;
+        const totalElement = document.getElementById("total");
+        if (totalElement) {
+            totalElement.innerHTML = this.paid.toFixed(2);
+        }
+    }
 }
